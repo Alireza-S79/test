4b3b2981ccb8abf842b0784f86bc552b121fe5fb
Alireza Safarzadeh <6441358-Alireza_S@users.noreply.gitlab.com>
2020-09-02 16:45:24 +0430

    iran added part 2

---
+++ app/src/main/java/com/example/UI/Activities/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.UI.Activities;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+import com.example.ViewModels.EgyptViewModel;
+import com.example.covid19tracker.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigationView = findViewById(R.id.navigation);
+
+        EgyptViewModel egyptViewModel = new ViewModelProvider(this).get(EgyptViewModel.class);
+        navigationView.setItemIconTintList(null);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        navigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.egyptFragment) {
+                    if (egyptViewModel.getIsLoading().getValue() != null
+                            && !egyptViewModel.getIsLoading().getValue()) {
+                        egyptViewModel.refresh();
+                    }
+                }
+            }
+        });
+    }
+}
